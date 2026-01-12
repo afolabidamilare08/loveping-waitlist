8d95929d6706baaecf01bc10ca7cac003974e592
Emmanuel Anyigor <emanuelanyigor@gmail.com>
2026-01-12 10:32:44 +0100

    updating toaster icon

--- src/components/Toaster.tsx
+++ src/components/Toaster.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react"
 import { subscribe, type ToastItem } from "./toast"
-import { FiTrash2 } from "react-icons/fi"
-import { FaCheck } from "react-icons/fa"
 
 const Toaster = () => {
     const [toasts, setToasts] = useState<ToastItem[]>([])
@@ -20,10 +18,26 @@ const Toaster = () => {
                     `}
                 >
                     <div className="flex items-center justify-center w-8 h-8 rounded-full bg-primary/10 border-[0.5px] border-primary/10">
-                        {toast.type === "success" ? (
-                            <FaCheck className="size-4 text-primary" />
+                        {toast.type === "error" ? (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="18"
+                                height="18"
+                                fill="#48D962"
+                                viewBox="0 0 256 256"
+                            >
+                                <path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path>
+                                </svg>
                         ) : (
-                            <FiTrash2 className="size-4 text-primary" />
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="18"
+                                height="18"
+                                fill="#48D962"
+                                viewBox="0 0 256 256"
+                            >
+                                <path d="M229.66,77.66l-128,128a8,8,0,0,1-11.32,0l-56-56a8,8,0,0,1,11.32-11.32L96,188.69,218.34,66.34a8,8,0,0,1,11.32,11.32Z"></path>
+                            </svg>
                         )}
                     </div>
                     {toast.message}
